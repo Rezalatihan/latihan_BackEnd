c77caaea87344dbaf8342d3d305e9d328c2e0dd8
rezalatihan <rezacatria1@gmail.com>
2022-10-04 10:34:25 -0700

    belajar backend

--- App.js
+++ App.js
@@ -2,8 +2,6 @@ const express = require('express')
 const app = express()
 const router = require('./routes')
 const user = require('./user')
-
 app.use(router)
 app.use(user)
-
 app.listen(3000, () => console.log('localhost:3000'))
